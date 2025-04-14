c58bed32561d8bcf50d3f0afc757f8939398cfe4
david <dpoulin45@gmail.com>
2025-04-14 18:40:17 +0200

    Change migration user_ddl.sql

--- app/core/migrations/ddl_users.sql
+++ app/core/migrations/ddl_users.sql
@@ -2,12 +2,15 @@
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
+  `username` varchar(4) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `nom` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) DEFAULT NULL,
   `role` enum('user','superuser','admin') DEFAULT 'user',
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
   `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `users_unique` (`nom`,`prenom`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
