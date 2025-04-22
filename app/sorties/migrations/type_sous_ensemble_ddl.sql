9621936c0a1d5adf3be5529e7209a9623ea05c5b
david <dpoulin45@gmail.com>
2025-04-22 18:51:06 +0200

    WIP Suivis - Initial Commit

--- app/sorties/migrations/type_sous_ensemble_ddl.sql
+++
@@ -1,7 +0,0 @@
--- `gestprod-v2`.type_sous_emsemble definition
-
-CREATE TABLE `type_sous_emsemble` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `designation` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
