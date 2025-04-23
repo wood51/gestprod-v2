908ff91cd4006df45d0bcdcf816099cd975a28b0
david <dpoulin45@gmail.com>
2025-04-23 22:49:41 +0200

    WIP: Menu HTMX

--- app/core/controllers/CoreController.php
+++ app/core/controllers/CoreController.php
@@ -8,7 +8,20 @@ class CoreController
     {
         $theme = "base";
         $f3->user = AuthService::user();
+        $f3->aside_menu = \Template::instance()->render("themes/base/partials/_nav_principale.html");
         echo \Template::instance()->render("themes/$theme/" . $theme . "_layout.html");
+    }
+
+    /**
+     * @route("GET /accueil-partials")
+     */
+    function accueil_partial($f3)
+    {
+        $menu = \Template::instance()->render("themes/base/partials/_nav_principale.html");
+        $content ="";
+
+        echo '<div id="aside_menu">' . $menu . '</div>';
+        echo '<div id="content" class="p-4">'.$content.'</div>';
     }
 
     /**
@@ -16,7 +29,7 @@ class CoreController
      */
     function toastMessages($f3)
     {
-        $f3->flash_messages= \Flash::instance()->getMessages();
+        $f3->flash_messages = \Flash::instance()->getMessages();
         echo \Template::instance()->render('themes/base/components/toast/_toast_messages.html');
     }
 }
