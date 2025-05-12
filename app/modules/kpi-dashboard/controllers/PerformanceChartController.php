b79cb6da6efa0f727bcd6da06d24e2bef3914cb9
David Poulin <dpoulin45@gmail.com>
2025-05-13 00:24:22 +0200

    WIP Performance OK

--- app/modules/kpi-dashboard/controllers/PerformanceChartController.php
+++ app/modules/kpi-dashboard/controllers/PerformanceChartController.php
@@ -1,14 +1,15 @@
 <?php
-class PerformanceChartController {
+class PerformanceChartController
+{
     protected $service;
 
     public function __construct()
     {
         $this->service = new PerformanceChartService();
     }
-    
 
-     /**
+
+    /**
      * @route("GET /performance/@semaine")
      */
     public function performanceActuel($f3, $params)
@@ -22,19 +23,4 @@ class PerformanceChartController {
             $f3->error(500, "Erreur lors du calcul de la performance actuelle: " . $e->getMessage());
         }
     }
-
-    /**
-     * @route("GET /test/presence")
-     */
-    function test_presence() {
-        $result  = PresenceModel::getNbOperateursSemaine(18, 2025, $heures_semaine = 35);
-        echo var_dump($result);
-        echo "<br>";
-        echo var_dump(ProdEngagementModel::getNbMachinePretes("2025-18"));
-        echo "<br>";
-        echo var_dump($this->service->calculPerformance("2025-18"));
-        echo "<br>";
-    }
-
-    
 }
