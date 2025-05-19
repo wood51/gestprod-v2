4460bc7b45587b123b438f674cd6584d4beff518
David Poulin <dpoulin45@gmail.com>
2025-05-19 06:04:37 +0200

    WIP

--- app/modules/kpi-dashboard/controllers/ChartController.php
+++ app/modules/kpi-dashboard/controllers/ChartController.php
@@ -6,6 +6,8 @@ class ChartController
      */
     function kpi_dashboad($f3)
     {
+        $f3->week="2025-20";
+        $f3->nb_semaine = 5;
         echo \template::instance()->render("kpi-dashboard/kpi_dashboard.html");
     }
 
