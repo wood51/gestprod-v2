2aa56748e6c1371232790b25f977e6db64020e28
wood51 <dpoulin45@gmail.com>
2025-05-25 10:20:31 +0200

    Update:semaine

--- app/modules/kpi-dashboard/controllers/ChartController.php
+++ app/modules/kpi-dashboard/controllers/ChartController.php
@@ -6,7 +6,7 @@ class ChartController
      */
     function kpi_dashboad($f3)
     {
-        $f3->week = "2025-22";//(DateHelper::build(2025,21))->full_week;
+        $f3->week = "2025-23";//(DateHelper::build(2025,21))->full_week;
         $f3->nb_semaine = 5;
         echo \template::instance()->render("kpi-dashboard/kpi_dashboard.html");
     }
